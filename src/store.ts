9464c0c594ad8eb8dc292d9e606ea630c5a2ba3c
Octav Codrea <56611209+octavcodrea@users.noreply.github.com>
2023-02-04 16:33:51 +0200

    palette tweaks

--- src/store.ts
+++ src/store.ts
@@ -13,8 +13,8 @@ type Store = {
 export const store = create<Store>((set) => ({
     seed: "0123456789012345",
 
-    canvasWidth: 5000,
-    canvasHeight: 6000,
+    canvasWidth: 2000,
+    canvasHeight: 2400,
 
     selectedPalette: 0,
     devMode: true,
